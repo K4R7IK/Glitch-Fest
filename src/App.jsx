9b763ab847d76101f398d916f996a20d814d29fc
ashish-kus <ashish.kus2408@gmail.com>
2024-02-18 03:10:48 +0530

    Merge conflict resolved

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Nav from "./components/nav/Nav";
 import Events from "./components/Events";
 import AboutGBU from "./components/AboutGBU";
 import AboutGlitch from "./components/AboutGlitch";
+import Footer from "./components/Footer";
+import Sponsor from "./components/soponsor";
+import Demographs from "./components/Demographs";
+import BgGlitch from './assets/Videos/skull3.gif';
 
 function App() {
   return (
@@ -32,7 +36,10 @@ function App() {
           src={BgVid}
           className="-z-1 absolute h-full w-full object-cover opacity-50"
         />
+        <Demographs />
         <Events />
+        <Sponsor />
+        <Footer />
       </div>
     </>
   );
